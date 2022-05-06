94183a871db17effa713c08469e5c48ddcd2ae73
Edgar Ngwenya <edgar@cuvama.com>
2022-05-06 00:00:19 -0400

    VDT-2327: Fix lambda configuration

--- qa-engineer/terraform-lambda/lambda.tf
+++ qa-engineer/terraform-lambda/lambda.tf
@@ -81,6 +81,6 @@ resource "aws_lambda_function" "lambda_functions" {
   role          = aws_iam_role.iam_for_lambda.arn
   runtime       = "nodejs14.x"
   timeout       = 5
-  memory_size   = 1024
+  memory_size   = 128
   handler       = "handler.handler"
 }
