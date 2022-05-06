295df1f47b97d8c0e383b5f0c9bb9fb58191be58
Edgar Ngwenya <edgar@cuvama.com>
2022-05-05 22:57:24 -0400

    VDT-2327: Add basic auth to backend

--- qa-engineer/backend/src/app.js
+++ qa-engineer/backend/src/app.js
@@ -3,8 +3,9 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const healthCheckRoutes = require("./routers/healthcheck.router");
 const tasksRoutes = require("./routers/tasks.router");
-const { validateRequestStructure, errorHandler } = require("./middleware");
+const {validateRequestStructure, errorHandler} = require("./middleware");
 const mongoose = require("mongoose");
+const basicAuth = require('express-basic-auth')
 
 const app = express();
 app.enable("trust proxy");
@@ -13,17 +14,28 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
 
+
+if (process.env.BASIC_AUTH_USERNAME) {
+    console.log("Basic Auth", process.env.BASIC_AUTH_USERNAME)
+    const users = {};
+    users[process.env.BASIC_AUTH_USERNAME] = process.env.BASIC_AUTH_PASSWORD;
+
+    app.use(basicAuth({
+        users
+    }));
+}
+
 app.use(validateRequestStructure);
 app.use("/healthcheck", healthCheckRoutes);
 app.use("/tasks", tasksRoutes);
 app.use(errorHandler);
 
 mongoose
-  .connect(
-    process.env.MONGODB_URL ||
-      "mongodb://localhost:27017/task?readPreference=primary&ssl=false"
-  )
-  .then(() => console.log("Connected"))
-  .catch(() => process.exit(1));
+    .connect(
+        process.env.MONGODB_URL ||
+        "mongodb://localhost:27017/task?readPreference=primary&ssl=false"
+    )
+    .then(() => console.log("Connected"))
+    .catch(() => process.exit(1));
 
 module.exports = app;
