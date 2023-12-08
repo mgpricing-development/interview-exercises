ba8925439cdc1bb40b926fa01f9bf10ade0ddea2
Edgar Ngwenya <edgar@cuvama.com>
2023-12-08 13:28:33 +0000

    Remove basic auth

--- qa-engineer/backend/src/app.js
+++ qa-engineer/backend/src/app.js
@@ -6,7 +6,6 @@ const configRoutes = require("./routers/config.router");
 const tasksRoutes = require("./routers/tasks.router");
 const { validateRequestStructure, errorHandler } = require("./middleware");
 const mongoose = require("mongoose");
-const basicAuth = require("express-basic-auth");
 
 const app = express();
 app.enable("trust proxy");
@@ -18,23 +17,6 @@ app.use(express.json());
 app.use(validateRequestStructure);
 app.use("/healthcheck", healthCheckRoutes);
 app.use("/config", configRoutes);
-
-if (process.env.BASIC_AUTH_USERNAME) {
-  console.log(
-    "Basic Auth",
-    process.env.BASIC_AUTH_USERNAME,
-    process.env.BASIC_AUTH_PASSWORD
-  );
-  const users = {};
-  users[process.env.BASIC_AUTH_USERNAME] = process.env.BASIC_AUTH_PASSWORD;
-
-  app.use(
-    basicAuth({
-      users
-    })
-  );
-}
-
 app.use("/tasks", tasksRoutes);
 app.use(errorHandler);
 
